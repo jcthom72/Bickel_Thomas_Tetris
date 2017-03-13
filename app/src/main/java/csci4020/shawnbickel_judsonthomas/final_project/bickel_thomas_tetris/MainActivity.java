6fb96873bc74a28a48163ba41b8923097fc717ed
student456773 <sbickel201@gmail.com>
2017-03-13 15:23:09 -0500

    Initial commit

---
+++ app/src/main/java/csci4020/shawnbickel_judsonthomas/final_project/bickel_thomas_tetris/MainActivity.java
@@ -0,0 +1,13 @@
+package csci4020.shawnbickel_judsonthomas.final_project.bickel_thomas_tetris;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
